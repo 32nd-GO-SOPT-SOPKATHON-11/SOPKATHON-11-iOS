d00f1866515461f93bcecc3167bc043a1a1be66f
정채은 <cce523@hanmail.net>
2023-05-21 01:18:12 +0900

    Feat [#8] HomeCollectionView 구현

---
+++ SOPKATHON-11-iOS/SOPKATHON-11-iOS/Presentation/View/Home/HomeCollectionViewCell.swift
@@ -0,0 +1,63 @@
+//
+//  HomeCollectionViewCell.swift
+//  SOPKATHON-11-iOS
+//
+//  Created by 정채은 on 2023/05/21.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class HomeCollectionViewCell: UICollectionViewCell {
+    
+    static let homeIdentifier = "HomeCollectionViewCell"
+    
+    let profileImageView = UIImageView().then {
+        $0.image = UIImage(systemName: "figure.bowling")
+    }
+    
+    let nameLabel = UILabel()
+    
+    let separateView = UIView()
+    
+    let bornLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setStyle()
+        setLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setStyle() {
+        self.backgroundColor = .clear
+    }
+    
+    private func setLayout() {
+        self.addSubviews(
+            profileImageView,
+            nameLabel,
+            separateView,
+            bornLabel
+        )
+        
+        self.snp.makeConstraints {
+            $0.width.equalTo(UIScreen.main.bounds.size.width / 2)
+            $0.height.equalTo(250)
+        }
+        
+        profileImageView.snp.makeConstraints{
+            $0.top.equalToSuperview()
+            $0.width.equalTo(UIScreen.main.bounds.width/2)
+            $0.height.equalTo(250)
+        }
+        
+    }
+}
